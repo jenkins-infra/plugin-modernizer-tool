468a962b40329d702daf1ad5373bdd28cb4e11db
Nagu <nagukalakuntla@gmail.com>
2025-01-03 19:22:55 +0530

    fixed errors in the UpdateScmUrlTest.java file

--- plugin-modernizer-core/src/test/java/io/jenkins/tools/pluginmodernizer/core/recipes/UpdateScmUrlTest.java
+++ plugin-modernizer-core/src/test/java/io/jenkins/tools/pluginmodernizer/core/recipes/UpdateScmUrlTest.java
@@ -2,41 +2,65 @@ package io.jenkins.tools.pluginmodernizer.core.recipes;
 
 import static org.openrewrite.maven.Assertions.pomXml;
 import org.junit.jupiter.api.Test;
-import org.openrewrite.test.RecipeSpec;
 import org.openrewrite.test.RewriteTest;
 
+/**
+ * Test for {@link UpdateScmUrl}.
+ */
 public class UpdateScmUrlTest implements RewriteTest {
-
-    @Override
-    public void defaults(RecipeSpec spec) {
-        spec.recipe(new UpdateScmUrl());
-    }
 
     @Test
     void updateScmUrls() {
         rewriteRun(
+          spec -> spec.recipe(new UpdateScmUrl()),
             // language=xml
-                pomXml(
-                        """
+            pomXml(
+                """
                 <?xml version="1.0" encoding="UTF-8"?>
-                <project xmlns="http://maven.apache.org/POM/4.0.0">
-                  <modelVersion>4.0.0</modelVersion>
-                  <scm>
-                    <url>git://github.com/example/repo.git</url>
-                    <connection>scm:git:git://github.com/example/repo.git</connection>
-                  </scm>
-                  <artifactId>example-plugin</artifactId>
+                <project xmlns="http://maven.apache.org/POM/4.0.0" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://maven.apache.org/POM/4.0.0 https://maven.apache.org/xsd/maven-4.0.0.xsd">
+                    <modelVersion>4.0.0</modelVersion>
+                    <groupId>io.jenkins.plugins</groupId>
+                    <artifactId>empty</artifactId>
+                    <version>1.0.0-SNAPSHOT</version>
+                    <packaging>hpi</packaging>
+                    <name>Empty Plugin</name>
+                    <properties>
+                        <jenkins.version>2.440.3</jenkins.version>
+                    </properties>
+                    <scm>
+                        <url>git://github.com/example/repo.git</url>
+                        <connection>scm:git:git://github.com/example/repo.git</connection>
+                    </scm>
+                    <repositories>
+                        <repository>
+                            <id>repo.jenkins-ci.org</id>
+                            <url>https://repo.jenkins-ci.org/public/</url>
+                        </repository>
+                    </repositories>
                 </project>
                 """,
-                        """
+                """
                 <?xml version="1.0" encoding="UTF-8"?>
-                <project xmlns="http://maven.apache.org/POM/4.0.0">
-                  <modelVersion>4.0.0</modelVersion>
-                  <scm>
-                    <url>https://github.com/example/repo.git</url>
-                    <connection>scm:git:https://github.com/example/repo.git</connection>
+                <project xmlns="http://maven.apache.org/POM/4.0.0" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://maven.apache.org/POM/4.0.0 https://maven.apache.org/xsd/maven-4.0.0.xsd">
+                    <modelVersion>4.0.0</modelVersion>
+                    <groupId>io.jenkins.plugins</groupId>
+                    <artifactId>empty</artifactId>
+                    <version>1.0.0-SNAPSHOT</version>
+                    <packaging>hpi</packaging>
+                    <name>Empty Plugin</name>
+                    <properties>
+                        <jenkins.version>2.440.3</jenkins.version>
+                    </properties>
+                    <scm>
+                        <url>https://github.com/example/repo.git</url>
+                        <connection>scm:git:https://github.com/example/repo.git</connection>
                     </scm>
-                  <artifactId>example-plugin</artifactId>
+                    <repositories>
+                        <repository>
+                            <id>repo.jenkins-ci.org</id>
+                            <url>https://repo.jenkins-ci.org/public/</url>
+                        </repository>
+                    </repositories>
                 </project>
                 """));
     }
@@ -44,18 +68,32 @@ public class UpdateScmUrlTest implements RewriteTest {
     @Test
     void keepExistingHttpsUrls() {
         rewriteRun(
+          spec -> spec.recipe(new UpdateScmUrl()),
             // language=xml
             pomXml(
-                    """
-            <?xml version="1.0" encoding="UTF-8"?>
-              <project xmlns="http://maven.apache.org/POM/4.0.0">
-                <modelVersion>4.0.0</modelVersion>
-                <scm>
-                  <url>https://github.com/example/repo.git</url>
-                  <connection>scm:git:https://github.com/example/repo.git</connection>
-                </scm>
-                <artifactId>example-plugin</artifactId>
-              </project>
-              """));
+                """
+                <?xml version="1.0" encoding="UTF-8"?>
+                <project xmlns="http://maven.apache.org/POM/4.0.0" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://maven.apache.org/POM/4.0.0 https://maven.apache.org/xsd/maven-4.0.0.xsd">
+                    <modelVersion>4.0.0</modelVersion>
+                    <groupId>io.jenkins.plugins</groupId>
+                    <artifactId>empty</artifactId>
+                    <version>1.0.0-SNAPSHOT</version>
+                    <packaging>hpi</packaging>
+                    <name>Empty Plugin</name>
+                    <properties>
+                        <jenkins.version>2.440.3</jenkins.version>
+                    </properties>
+                    <scm>
+                        <url>https://github.com/example/repo.git</url>
+                        <connection>scm:git:https://github.com/example/repo.git</connection>
+                    </scm>
+                    <repositories>
+                        <repository>
+                            <id>repo.jenkins-ci.org</id>
+                            <url>https://repo.jenkins-ci.org/public/</url>
+                        </repository>
+                    </repositories>
+                </project>
+                """));
     }
 }
