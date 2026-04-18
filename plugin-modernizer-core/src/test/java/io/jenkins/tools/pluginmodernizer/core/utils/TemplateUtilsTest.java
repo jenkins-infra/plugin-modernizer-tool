c5c252629279a954df2c785c20d022d76719d0f1
Valentin Delaye <jonesbusy@users.noreply.github.com>
2026-04-18 15:12:36 +0200

    Add new recipe to migrate plugin to minium Java 21 (2.555.1)

--- plugin-modernizer-core/src/test/java/io/jenkins/tools/pluginmodernizer/core/utils/TemplateUtilsTest.java
+++ plugin-modernizer-core/src/test/java/io/jenkins/tools/pluginmodernizer/core/utils/TemplateUtilsTest.java
@@ -306,6 +306,27 @@ public class TemplateUtilsTest {
     }
 
     @Test
+    public void testFriendlyPrTitleBaseLineToJenkinsMinimumRequiredJava21Version() {
+
+        // Mocks
+        Plugin plugin = mock(Plugin.class);
+        PluginMetadata metadata = mock(PluginMetadata.class);
+        Recipe recipe = mock(Recipe.class);
+
+        doReturn(metadata).when(plugin).getMetadata();
+        doReturn("2.555.1").when(metadata).getJenkinsVersion();
+        doReturn("io.jenkins.tools.pluginmodernizer.BaseLineToJenkinsMinimumRequiredJava21Version")
+                .when(recipe)
+                .getName();
+
+        // Test
+        String result = TemplateUtils.renderPullRequestTitle(plugin, recipe);
+
+        // Assert
+        assertEquals("feat(java): Require Jenkins core 2.555.1 and Java 21", result);
+    }
+
+    @Test
     public void testFriendlyPrTitleMigrateToJenkinsBaseLineProperty() {
 
         // Mocks
@@ -645,6 +666,28 @@ public class TemplateUtilsTest {
         // Assert
         assertTrue(result.contains("Why is this important?"), "Missing 'Why is this important?' section");
         assertTrue(result.contains("ban-deprecated-stapler.skip"), "Missing property name");
+    }
+
+    @Test
+    public void testFriendlyPrBodyBaseLineToJenkinsMinimumRequiredJava21Version() {
+
+        // Mocks
+        Plugin plugin = mock(Plugin.class);
+        PluginMetadata metadata = mock(PluginMetadata.class);
+        Recipe recipe = mock(Recipe.class);
+
+        doReturn(metadata).when(plugin).getMetadata();
+        doReturn("io.jenkins.tools.pluginmodernizer.BaseLineToJenkinsMinimumRequiredJava21Version")
+                .when(recipe)
+                .getName();
+
+        // Test
+        String result = TemplateUtils.renderPullRequestBody(plugin, recipe);
+
+        // Assert
+        assertTrue(
+                result.contains("Use Java 21 feature and APIs on your plugin"),
+                "Missing 'Use Java 21 feature and APIs on your plugin' message");
     }
 
     @Test
